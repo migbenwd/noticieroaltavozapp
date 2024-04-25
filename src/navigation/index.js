74b544873f2cc0146209bb0dfa818c5ef32c7a40
migben.com <miguelangel.benitezyanez@gmail.com>
2024-04-25 11:11:38 -0400

    funciona excelente en local - se vuelve a colocar icono home en azul

--- src/navigation/index.js
+++ src/navigation/index.js
@@ -43,7 +43,7 @@ export default function AppNavigation() {
               <Ionicons
                 name={iconName}
                 size={customizeSize}
-                color={focused ? 'red' : 'gray'}
+                color={focused ? 'blue' : 'gray'}
               />
             );
           },
