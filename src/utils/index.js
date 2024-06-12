d066eeae185ac34d25629d5ce3db87b4bb776f5e
migben.com <miguelangel.benitezyanez@gmail.com>
2024-06-12 15:35:50 -0400

    func excel: se agrega condicional de "click" o "sin click" del master slider segun wp

--- src/utils/index.js
+++ src/utils/index.js
@@ -3,24 +3,29 @@
 export const extractImagesWithTheirSource = (htmlString) => {
   const imgRegex = /<img.*?data-src="([^"]+)".*?>/g;
   const linkRegex = /<a.*?href="([^"]+)".*?>/g;
+  const claseClick = /<div.*?class="([^"]+)".*?>/g;
 
   const images = Array.from(htmlString.matchAll(imgRegex), (match) => match[1]);
   const urls = Array.from(htmlString.matchAll(linkRegex), (match) => match[1]);
+  const likysx = Array.from(
+    htmlString.matchAll(claseClick),
+    (match) => match[1]
+  );
 
-  console.log('Number of images:', images.length);
-  console.log('Number of URLs:', urls.length);
+  const filteredArray = likysx.filter(
+    (element) => element.includes('sin-click') || element.includes('con-click')
+  );
+  const ConSinClickArray = filteredArray.map((item) =>
+    item.replace('ms-slide ', '')
+  );
 
-  console.log('images');
-  console.log(images);
-
-  console.log('urls');
-  console.log(urls);
-
-  // Implementar logica de si o no vacio en URL
-  // aqui se hace un nuevo map
-  // martes 11-jun 2024 - 4:48 pm
+  console.log(ConSinClickArray);
 
   return images.map((item, index) => {
+    console.log('imagen:', images[index]);
+    console.log('url:', urls[index]);
+    // console.log('url:', urls[index]);
+
     if (images.length === urls.length) {
       return {
         image: item,
