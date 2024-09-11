da46544a8bc2887cd741fb63736969ffdc0f3831
migben.com <miguelangel.benitezyanez@gmail.com>
2024-09-11 14:43:47 -0400

    func excel: aun sin one signal

--- App.js
+++ App.js
@@ -1,18 +1,17 @@
 /* eslint-disable react/jsx-no-bind */
 /* eslint-disable react/no-unstable-nested-components */
 
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 
-import AppNavigation from "./src/navigation";
-import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import React from 'react';
+import AppNavigation from './src/navigation';
+
 const queryClient = new QueryClient();
 
-import React from "react";
-
 export default function App() {
-
   return (
     <QueryClientProvider client={queryClient}>
       <AppNavigation />
     </QueryClientProvider>
   );
-} 
+}
