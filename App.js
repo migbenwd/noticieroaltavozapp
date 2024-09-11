67be29c230620c1e60cbe34ddb3d5c1ddf1f4e52
migben.com <miguelangel.benitezyanez@gmail.com>
2024-09-11 10:52:30 -0400

    subiendo integracion One Signal

--- App.js
+++ App.js
@@ -6,7 +6,18 @@ import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import React from 'react';
 import AppNavigation from './src/navigation';
 
+//One Signal
+import { LogLevel, OneSignal } from 'react-native-onesignal';
+import Constants from 'expo-constants'
+
 const queryClient = new QueryClient();
+
+OneSignal.Debug.setLogLevel(LogLevel.Verbose);
+OneSignal.initialize(Constants.expoConfig.extra.oneSignalAppId);
+
+// Also need enable notifications to complete OneSignal setup
+OneSignal.Notifications.requestPermission(true);
+
 
 export default function App() {
   return (
