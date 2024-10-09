9480ca5bf63f5d3108cea58907aaa2d1ed6b3bfb
migben.com <miguelangel.benitezyanez@gmail.com>
2024-10-09 09:35:09 -0400

    func excel - 09-oct - 9:34 am

--- App.js
+++ App.js
@@ -2,6 +2,7 @@
 /* eslint-disable react/no-unstable-nested-components */
 
 // func excel - 25-sep - 6:47 pm
+// func excel - 09-oct - 9:34 am
 
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 
