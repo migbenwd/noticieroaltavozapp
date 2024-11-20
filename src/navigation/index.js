31d7686539a69e53e24ed27b15b5aca8c115c731
migben.com <miguelangel.benitezyanez@gmail.com>
2024-11-20 10:02:55 -0400

    func excel: pero sigue recibiendo la misma URL

--- src/navigation/index.js
+++ src/navigation/index.js
@@ -114,86 +114,35 @@ function TabNavigator() {
   );
 }
 
-// OneSignal.initialize('8497271c-4edb-486f-a683-063bd6205b5b');
+OneSignal.initialize('8497271c-4edb-486f-a683-063bd6205b5b');
 
 export default function AppNavigation() {
   const { colorScheme } = useColorScheme();
   const tituloCategoria = 'Portada';
-
-  const [eventData, setEventData] = useState(null);
-  const [EnlaceURL, setEnlaceURL] = useState(null);
-
-  OneSignal.initialize('8497271c-4edb-486f-a683-063bd6205b5b');
-
   useEffect(() => {
-    // Inicializar OneSignal (si es necesario)
-    // OneSignal.init('tu_app_id');
+    const subscription = OneSignal.Notifications.addEventListener(
+      'click',
+      (event) => {
+        console.log('POST URL de Event es ....................');
+        console.log(event.notification.additionalData.post_url);
+        const EnlaceURL = {
+          link: event.notification.additionalData.post_url,
+        };
 
-    // Escuchar el evento de clic en la notificación
-    OneSignal.Notifications.addEventListener('click', (openResult) => {
-      console.log('... 1 - openResult...');
-      console.log(openResult.notification.additionalData.post_url);
-      console.log('.............///.....');
-
-      // const { notification } = openResult.notification.additionalData.post_url;
-      // const { notification } = openResult;
-      const { notification } = openResult;
-      console.log('...notification...');
-      console.log(notification.additionalData.post_url);
-
-      setEventData(notification);
-      setEnlaceURL({ link: notification });
-    });
-
-    // Limpiar el listener al desmontar el componente
-    return () => {
-      OneSignal.Notifications.removeEventListener('click');
-    };
-  }, []);
-
-  useEffect(() => {
-    if (eventData) {
-      console.log('Entra en UseEffect 2');
-      console.log('eventData tiene: ');
-      console.log(eventData);
-      // Navegar a la pantalla deseada con los datos de la notificación
-      navigationRef.current.navigate('NewsDetailsMigben', {
-        item: EnlaceURL,
-        tituloCategoria,
-      });
-    }
-  }, [eventData]);
-
-  /*
-  useEffect(() => {
-    OneSignal.Notifications.addEventListener('click', (event) => {
-      const notifix = event.notification.additionalData.post_url;
-      setPost(event.notification.additionalData.post_url);
-      console.log('..... post-1..... ');
-      console.log(post);
-
-      const EnlaceURL = {
-        link: notifix,
-      };
-
-      console.log('..... post-2..... ');
-      setPost(null);
-      console.log(post);
-
-      if (navigationRef.isReady()) {
-        console.log('ESTA READY y el valor de post es: ');
-        console.log(post);
-
-        navigationRef.dispatch(
-          CommonActions.navigate('NewsDetailsMigben', {
+        // Verifica y navega a la pantalla NewsDetails pasando los datos de la notificación
+        if (navigationRef.isReady()) {
+          navigationRef.current.navigate('NewsDetails', {
             item: EnlaceURL,
             tituloCategoria,
-          })
-        );
+          });
+        }
       }
-    });
+    );
+
+    return () => {
+      subscription.remove();
+    };
   }, []);
-  */
 
   return (
     <NavigationContainer ref={navigationRef}>
