df81541cb6da98ca872943c9400564d89021c000
migben.com <miguelangel.benitezyanez@gmail.com>
2024-12-29 13:41:16 -0400

    funciona en modo DEVELOPMENT en celular REDMI (ELENA)

--- App.js
+++ App.js
@@ -6,6 +6,7 @@
 // func excel - 09-oct - 9:34 am
 // probando en preview
 // - probar generar preview y development
+// funciona en modo DEVELOPMENT en celular REDMI (ELENA)
 
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 // import { LogLevel, OneSignal } from 'react-native-onesignal';
