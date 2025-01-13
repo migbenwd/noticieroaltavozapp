7f6206bbc42dc0ca1848ff634b4837d3d16db9aa
migben.com <miguelangel.benitezyanez@gmail.com>
2025-01-13 10:31:59 -0400

    fix index.js

--- src/navigation/index.js
+++ src/navigation/index.js
@@ -3,10 +3,6 @@
 /* eslint-disable import/no-extraneous-dependencies */
 /* eslint-disable react/no-unstable-nested-components */
 
-// migben - cambiar menu radical
-// migben jueves - 08:20 am
-// migben martes 9-dic - 07:02 pm - esta version tiene comentado el codigo de one signal y por ende, no funciona las push notifications
-// migben martes 7-ene-2025 ... 8:00 pm
 //.
 
 import React, { useEffect } from 'react';
