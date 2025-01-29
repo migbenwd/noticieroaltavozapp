7781750f3e9fa19291a7aa54e7f5451ed7cf764f
migben.com <miguelangel.benitezyanez@gmail.com>
2025-01-29 15:42:09 -0400

    version para cambiar radio

--- src/screens/RadioScreen.js
+++ src/screens/RadioScreen.js
@@ -1,267 +1,127 @@
-/* eslint-disable global-require */
-
 import React, { useEffect, useState } from 'react';
-import {
-  // SafeAreaView,
-  // ScrollView,
-  StyleSheet,
-  Text,
-  View,
-  FlatList,
-  ActivityIndicator,
-  TouchableOpacity,
-  Image,
-} from 'react-native';
+import { View, Text, TouchableOpacity, StyleSheet } from 'react-native';
 import TrackPlayer, {
-  useTrackPlayerEvents,
-  usePlaybackState,
-  // useProgress,
-  Event,
+  Capability,
   State,
+  usePlaybackState,
 } from 'react-native-track-player';
-// import Icon from 'react-native-vector-icons/FontAwesome';
-import Icon from 'react-native-vector-icons/FontAwesome';
-import { SafeAreaView } from 'react-native-safe-area-context';
-import { useColorScheme } from 'nativewind';
-import { StatusBar } from 'expo-status-bar';
 
-import {
-  useFonts,
-  Poppins_400Regular,
-  Poppins_700Bold,
-} from '@expo-google-fonts/poppins';
-import { getPlaybackState } from 'react-native-track-player/lib/src/trackPlayer';
-import { setupPlayer, addTracks } from '../../trackPlayerServices';
+const radioStations = [
+  {
+    id: '1',
+    url: 'https://streaming.shoutcast.com/radio-65',
+    title: 'RADIO 65',
+    duration: 66,
+    artwork:
+      'https://noticieroaltavoz.com/wp-content/uploads/2024/01/RADIO-65-BLANCO.png',
+  },
+  {
+    id: '2',
+    url: 'https://streaming.shoutcast.com/gs-la-super-estacion',
+    title: 'LA GS SUPER ESTACIÓN',
+    duration: 66,
+    artwork:
+      'https://noticieroaltavoz.com/wp-content/uploads/2024/01/LA-GS-BLANCO.png',
+  },
+  {
+    id: '3',
+    url: 'https://streaming.shoutcast.com/la-jl',
+    title: 'LA JL',
+    duration: 66,
+    artwork:
+      'https://noticieroaltavoz.com/wp-content/uploads/2024/01/LA-JL-BLANCO.png',
+  },
+  {
+    id: '4',
+    url: 'https://streaming.shoutcast.com/la-maxi-gml',
+    title: 'LA MAXI GML',
+    duration: 73,
+    artwork:
+      'https://noticieroaltavoz.com/wp-content/uploads/2024/01/A-MAXI-GML-1.png',
+  },
+  {
+    id: '5',
+    url: 'https://streaming.shoutcast.com/la-maxi',
+    title: 'LA MAXI',
+    duration: 73,
+    artwork:
+      'https://noticieroaltavoz.com/wp-content/uploads/2024/01/A-MAXI-LOGO-CONTORNO-BCO-1.png',
+  },
+];
 
-function NombreEmisora() {
-  const [info, setInfo] = useState({});
+const setupPlayer = async () => {
+  await TrackPlayer.setupPlayer();
+  await TrackPlayer.updateOptions({
+    stoppingAppPausesPlayback: true,
+    capabilities: [Capability.Play, Capability.Pause, Capability.Stop],
+  });
+};
+
+function RadioApp() {
+  const playbackState = usePlaybackState();
+  const [currentStationIndex, setCurrentStationIndex] = useState(0);
+  const [playStatus, setPlayStatus] = useState('DETENIDO'); // Cambia el estado inicial a 'DETENIDO'
+
   useEffect(() => {
-    setTrackInfo();
+    setupPlayer();
   }, []);
 
-  useTrackPlayerEvents([Event.PlaybackTrackChanged], (event) => {
-    if (event.state == State.nextTrack) {
-      setTrackInfo();
-    }
-  });
-
-  async function setTrackInfo() {
-    const track = await TrackPlayer.getCurrentTrack();
-    const info = await TrackPlayer.getTrack(track);
-    setInfo(info);
-  }
-
-  return (
-    <View>
-      <Text style={styles.songTitle}>{info.title}</Text>
-    </View>
-  );
-}
-
-function Playlist() {
-  const [queue, setQueue] = useState([]);
-  const [currentTrack, setCurrentTrack] = useState(0);
-
-  async function loadPlaylist() {
-    const queue = await TrackPlayer.getQueue();
-    setQueue(queue);
-  }
-
-  useEffect(() => {
-    loadPlaylist();
-  }, []);
-
-  useTrackPlayerEvents([Event.PlaybackTrackChanged], (event) => {
-    if (event.state == State.nextTrack) {
-      TrackPlayer.getCurrentTrack().then((index) => setCurrentTrack(index));
-    }
-  });
-
-  function PlaylistItem({ index, isCurrent, logoemisora }) {
-    function handleItemPress() {
-      TrackPlayer.skip(index);
-    }
-    return (
-      <TouchableOpacity onPress={handleItemPress}>
-        <Image
-          style={{
-            resizeMode: 'contain',
-            height: 120,
-            width: 120,
-            marginLeft: 20,
-            marginTop: 10,
-            marginBottom: 1,
-            borderRadius: 12,
-            borderColor: 'gray',
-            borderWidth: 1,
-
-            ...{
-              backgroundColor: isCurrent ? '#7070E7' : '#0404B2',
-            },
-          }}
-          source={{
-            uri: logoemisora,
-          }}
-        />
-      </TouchableOpacity>
-    );
-  }
-
-  async function handleShuffle() {
-    const queue = await TrackPlayer.getQueue();
+  const playStation = async (index) => {
     await TrackPlayer.reset();
-    queue.sort(() => Math.random() - 0.5);
-    await TrackPlayer.add(queue);
+    await TrackPlayer.add({
+      id: radioStations[index].id,
+      url: radioStations[index].url,
+      title: radioStations[index].title,
+      artist: 'Live Stream',
+    });
+    await TrackPlayer.play();
+    setCurrentStationIndex(index);
+    setPlayStatus('SONANDO'); // Actualiza el estado a 'SONANDO' cuando se inicia la reproducción
+  };
 
-    loadPlaylist();
-  }
-
-  return (
-    <View>
-      <View
-        style={{
-          justifyContent: 'center',
-          alignItems: 'center',
-        }}
-      >
-        <Text style={styles.nombreAviso}>
-          Escucha la <Text style={{ fontWeight: 'bold' }}> radio en vivo</Text>
-        </Text>
-      </View>
-
-      <View style={styles.playlist}>
-        <FlatList
-          horizontal={false}
-          numColumns={2}
-          contentContainerStyle={{
-            justifyContent: 'center',
-            alignItems: 'center',
-          }}
-          data={queue}
-          renderItem={({ item, index }) => (
-            <PlaylistItem
-              index={index}
-              title={item.title}
-              logoemisora={item.artwork}
-              isCurrent={currentTrack == index}
-            />
-          )}
-        />
-      </View>
-
-      <View
-        style={{
-          justifyContent: 'center',
-          alignItems: 'center',
-        }}
-      >
-        <NombreEmisora />
-        <Controls onShuffle={handleShuffle} />
-      </View>
-    </View>
-  );
-}
-
-function Controls({ onShuffle }) {
-  const playerState = usePlaybackState();
-
-  async function handlePlayPress() {
-    if ((await TrackPlayer.getState()) === State.Playing) {
-      // if ((await getPlaybackState()) === State.Paused)
-      TrackPlayer.pause();
+  const togglePlayPause = async () => {
+    const state = await TrackPlayer.getState();
+    if (state === State.Playing) {
+      await TrackPlayer.pause();
+      setPlayStatus('DETENIDO'); // Actualiza el estado a 'DETENIDO' cuando se pausa
     } else {
-      TrackPlayer.play();
+      await TrackPlayer.play();
+      setPlayStatus('SONANDO'); // Actualiza el estado a 'SONANDO' cuando se reanuda
     }
-  }
+  };
+
+  const nextStation = () => {
+    const nextIndex = (currentStationIndex + 1) % radioStations.length;
+    playStation(nextIndex);
+  };
+
+  const prevStation = () => {
+    const prevIndex =
+      (currentStationIndex - 1 + radioStations.length) % radioStations.length;
+    playStation(prevIndex);
+  };
 
   return (
-    <View
-      style={{
-        flexDirection: 'row',
-        // justifyContent: 'space-around',
-        // backgroundColor: 'red',
-        padding: 0,
-      }}
-    >
-      <View style={styles.button}>
-        <Icon
-          name="arrow-left"
-          size={25}
-          color="white"
-          onPress={() => TrackPlayer.skipToPrevious()}
-        />
-      </View>
-
-      <View style={styles.button}>
-        <Icon
-          name={playerState.state === 'paused' ? 'play' : 'pause'}
-          size={25}
-          color="white"
-          onPress={handlePlayPress}
-        />
-      </View>
-
-      <View style={styles.button}>
-        <Icon
-          name="arrow-right"
-          size={25}
-          color="white"
-          onPress={() => TrackPlayer.skipToNext()}
-        />
+    <View style={styles.container}>
+      <Text style={styles.title}>Radio App</Text>
+      <Text style={styles.stationTitle}>
+        {radioStations[currentStationIndex].title}
+      </Text>
+      <Text style={styles.statusText}>{playStatus}</Text>
+      {/* Nuevo texto para mostrar el estado actual */}
+      <TouchableOpacity style={styles.button} onPress={togglePlayPause}>
+        <Text style={styles.buttonText}>{playStatus}</Text>
+        {/* Botón actualizado para mostrar 'SONANDO' o 'DETENIDO' */}
+      </TouchableOpacity>
+      <View style={styles.controls}>
+        <TouchableOpacity onPress={prevStation} style={styles.button}>
+          <Text style={styles.buttonText}>⏮️ Prev</Text>
+        </TouchableOpacity>
+        <TouchableOpacity onPress={nextStation} style={styles.button}>
+          <Text style={styles.buttonText}>Next ⏭️</Text>
+        </TouchableOpacity>
       </View>
     </View>
-  );
-}
-
-export default function Radio() {
-  const [fontsLoaded] = useFonts({
-    Poppins_400Regular,
-    Poppins_700Bold,
-  });
-
-  const { colorScheme } = useColorScheme();
-
-  const [isPlayerReady, setIsPlayerReady] = useState(false);
-
-  useEffect(() => {
-    async function setup() {
-      const isSetup = await setupPlayer();
-
-      const queue = await TrackPlayer.getQueue();
-      if (isSetup && queue.length <= 0) {
-        await addTracks();
-      }
-
-      setIsPlayerReady(isSetup);
-    }
-
-    setup();
-  }, []);
-
-  if (!isPlayerReady) {
-    return (
-      <SafeAreaView style={styles.container}>
-        <ActivityIndicator size="large" color="#bbb" />
-      </SafeAreaView>
-    );
-  }
-
-  return (
-    <SafeAreaView style={{ flex: 1 }} edge={['bottom']}>
-      <View className="flex-row justify-between items-center px-2 pb-12 bg-[#0303B2]" />
-      <StatusBar style={colorScheme === 'dark' ? 'light' : 'dark'} />
-
-      <View className="items-center mb-2  bg-white">
-        <Image
-          source={require('../../assets/images/welcome/logo.png')}
-          style={{
-            resizeMode: 'contain',
-            width: '60%',
-          }}
-        />
-      </View>
-      <Playlist />
-    </SafeAreaView>
   );
 }
 
@@ -269,35 +129,40 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     justifyContent: 'center',
-    backgroundColor: 'blue',
-  },
-  songTitle: {
-    fontSize: 20,
-    marginBottom: 2,
-    marginTop: 7,
-    color: 'black',
-    textAlign: 'center',
-  },
-
-  playlist: {
-    marginTop: 0,
-  },
-
-  nombreAviso: {
-    // backgroundColor: 'blue',
-    justifyContent: 'center',
     alignItems: 'center',
-    fontSize: 20,
-    marginBottom: 5,
+    backgroundColor: '#121212',
   },
+  title: {
+    fontSize: 24,
+    color: '#fff',
+    marginBottom: 20,
+  },
+  stationTitle: {
+    fontSize: 20,
+    color: '#fff',
+    marginBottom: 10,
+  },
+  statusText: {
+    fontSize: 18,
+    color: '#fff',
+    marginBottom: 10,
+    backgroundColor: 'red',
 
+  },
   button: {
-    width: 45,
-    height: 45,
-    borderRadius: 25,
-    backgroundColor: 'blue',
-    justifyContent: 'center',
-    alignItems: 'center',
-    margin: 8,
+    backgroundColor: '#1DB954',
+    padding: 15,
+    borderRadius: 10,
+    marginTop: 10,
+  },
+  buttonText: {
+    color: '#fff',
+    fontSize: 16,
+  },
+  controls: {
+    flexDirection: 'row',
+    marginTop: 20,
   },
 });
+
+export default RadioApp;
