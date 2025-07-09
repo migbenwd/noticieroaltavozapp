a7540baa7d130f869153db238b3e2fbf5ce22b49
migben.com <miguelangel.benitezyanez@gmail.com>
2025-07-09 09:07:38 -0400

    cambio a inicio en HOME

--- src/navigation/index.js
+++ src/navigation/index.js
@@ -181,7 +181,7 @@ export default function AppNavigation() {
   return (
     <NavigationContainer>
       <Stack.Navigator
-        initialRouteName="Welcome"
+        initialRouteName="HomeTabs"
         screenOptions={{
           headerShown: false,
         }}
